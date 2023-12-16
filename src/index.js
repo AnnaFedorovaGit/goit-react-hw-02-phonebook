a9bd2293bc7a7fc920f329650974c86978da6aab
Anna Fedorova <134813806+AnnaFedorovaGit@users.noreply.github.com>
2023-12-16 20:04:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+import ReactDOM from 'react-dom/client';
+import App from 'App';
+import './index.css';
+
+
+const root = document.getElementById('root')
+ReactDOM.createRoot(root).render(<App />)
